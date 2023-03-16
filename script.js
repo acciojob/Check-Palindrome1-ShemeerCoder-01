a58888b982a2288ebc8a363b927446fc58559b12
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-03-16 14:15:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 // complete the given function
 
 function palindrome(str){
-	var len = str.length;
-	var i = 0;
-	var j = len - 1;
-	while(i < j){
-		if(str.charCodeAt(i) != str.charCodeAt(j)){
-			return false;
-		}
-		i++;
-		j++;
-	}
-	return true;
+  var regex = /[\W_]/g;
+  var lowRegStr = str.toLowerCase().replace(regex, '');
+  var reverseStr = lowRegStr.split('').reverse().join(''); 
+  return reverseStr === lowRegStr;
 	
 }
 module.exports = palindrome
